fe596144d9e8b352a31fb5f2cb98eb317330f1f8
deanhattenhauer <dean.hattenhauer@outlook.com>
2026-07-25 20:22:03 -0500

    feat: create user table

---
+++ sql/schema/20260726011320_create_users_table.sql
@@ -0,0 +1,12 @@
+-- +goose Up
+CREATE TABLE users (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    created_at TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP NOT NULL,
+    email TEXT NOT NULL UNIQUE,
+    hashed_password TEXT NOT NULL,
+    role TEXT NOT NULL DEFAULT 'user'
+);
+
+-- +goose Down
+DROP TABLE users;
