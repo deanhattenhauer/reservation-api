dce191069a7c3050cd4f94b24592c08a0c06ac92
deanhattenhauer <dean.hattenhauer@outlook.com>
2026-07-25 21:20:00 -0500

    fix: add is_active column to categories table

--- sql/schema/20260726012803_create_categories_table.sql
+++ sql/schema/20260726012803_create_categories_table.sql
@@ -3,7 +3,8 @@ CREATE TABLE categories (
 	id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
 	name TEXT NOT NULL UNIQUE,
 	created_at TIMESTAMP NOT NULL,
-	updated_at TIMESTAMP NOT NULL
+	updated_at TIMESTAMP NOT NULL,
+	is_active BOOLEAN NOT NULL DEFAULT true
 );
 
 -- +goose Down
