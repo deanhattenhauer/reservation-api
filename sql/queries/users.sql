1efd22a5eec4b37f666aab12643773ba8d84708b
ChristianBlackwell <christian.blackwell@outlook.com>
2026-07-31 14:26:59 -0500

    feat: create users query & generate type-safe internal/database files

---
+++ sql/queries/users.sql
@@ -0,0 +1,26 @@
+-- name: CreateUser :one
+INSERT INTO users (id, created_at, updated_at, email, hashed_password)
+VALUES (
+    gen_random_uuid(),
+    NOW(),
+    NOW(),
+    $1, 
+    $2
+)
+RETURNING *;
+
+-- name: GetUserByEmail :one
+SELECT * FROM users
+WHERE email = $1;
+
+-- name: UpdateUser :one
+UPDATE users
+SET email = $1, hashed_password = $2, updated_at = NOW()
+WHERE id = $3
+RETURNING *;
+
+-- name: UpdateUserRole :one
+UPDATE users
+SET role = $1, updated_at = NOW()
+WHERE id = $2
+RETURNING *;
